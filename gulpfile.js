e961ebcf174044320e0e71c0b0a55157d7673086
Adam Goforth <code@adamgoforth.com>
2015-10-30 01:10:03 +0100

    Add Gulp for CSS processing and minification

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+    minifyCss = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat');
+
+gulp.task('default', function() {
+    gulp.src('css/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ie 9'],
+            cascade: false
+        }))
+        .pipe(minifyCss())
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('.'));
+});
